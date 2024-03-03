5122896a484ec364c8867769ddd54c0518418575
amliang1 <amliang@emory.edu>
2024-03-03 17:02:19 -0500

    port

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+
+// Use the PORT environment variable provided by Heroku or a default value
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
@@ -45,6 +47,7 @@ app.post('/analyze-text', async (req, res) => {
     }
 });
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
