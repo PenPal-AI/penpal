affcf682a67ff82fc4f82d5bcc657640f1dd6ccb
Sofia Gukelberger <sofiagukelb@gmail.com>
2024-03-04 15:08:08 -0500

    fixed bug where generated suggestions weren't selectable

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ window.onload = () => {
   quill.clipboard.dangerouslyPasteHTML(cachedText);
 };
 
-var listItems = $(".list-group-item"); 
 
+
+var listItems = $(".list-group-item"); 
 
 //BUG/TODO: GENERATED SUGGESTIONS (VIA BUTTON) ARE NOT SELECTABLE. NO CLUE WHY THIS HAPPENS
 $(document).ready(function(){
@@ -35,17 +36,41 @@ $(document).ready(function(){
   //generate new suggestion
   //currently uses a button, but this is just for demo purposes
   //when we generate suggestions in the backend, call this function
-  $('.suggest').click(function() {
+  //$('.suggest').click(function() {
+      numOfSuggestions = 4;
       title = "new suggestion";  
       body = "body of suggestion";
       number = "22";
-      $('.list-group').append(" <a href='#' class='list-group-item list-group-item-action flex-column align-items-start'>" + 
+
+      for (let i = 0; i < numOfSuggestions; i++) { 
+        $('.list-group').append(" <a href='#' onclick='selectCard(\"elem" + (i+2).toString() + "\")' class='list-group-item list-group-item-action flex-column align-items-start elem" + (i+2).toString() + "'>" + 
                                 "<div class='d-flex w-100 justify-content-between'>" +
                                 "<h5 class='mb-1'>" + title + "</h5>" +
                                 "<span class='badge badge-primary badge-pill'>" + number + "</span> </div>" +
                                 "<p class='mb-1'>" + body + "!</p></a>");
-    });
+    }
+      
+    //});
   
 });
+
+
+function selectCard(elemNumber) {
+  // Select all list items 
+  listItems = $(".list-group-item"); 
+  console.log(listItems);
+    
+  // Remove 'active' tag for all list items 
+  for (let i = 0; i < listItems.length; i++) { 
+      listItems[i].classList.remove("active"); 
+  } 
+   
+  console.log(elemNumber);
+  var element = document.querySelector("." + elemNumber) //.getElementById(elemNumber);
+  console.log(element);
+
+  // Add 'active' tag for currently selected item 
+  element.classList.add("active"); 
+};
 
 
