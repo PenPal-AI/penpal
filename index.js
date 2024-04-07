7532e107b16ce8ba1d65ef39a41d1e352bc6b12e
Jordan Leslie <78113256+jordanlleslie@users.noreply.github.com>
2024-04-07 17:08:00 -0400

    Word frequency highlighting (#7)
    * highlight overused word on selection of corresponding card
    
    * unhighlight nonselected frequent words
    
    * fixed unhighlighting
    
    * refactor reset highlights
    
    * merge conflict

--- index.js
+++ index.js
@@ -20,12 +20,14 @@ function writeMode() {
   isEditMode = false;
   hideButton();
   deleteEditingSuggestions();
+  resetHighlights();
 }
 
 function suggestMode() {
   isEditMode = false;
   showButton();
   deleteEditingSuggestions();
+  resetHighlights();
 }
 
 function editMode() {
@@ -38,7 +40,14 @@ function editMode() {
 
 //filter for the text array
 function filterFunction(value) {
-    return (value != "" && value != "a" && value != "an" && value != "the" && value != "and" && value != "or");
+  return (
+    value != "" &&
+    value != "a" &&
+    value != "an" &&
+    value != "the" &&
+    value != "and" &&
+    value != "or"
+  );
 }
 
 //general list of items in suggestion list
@@ -49,36 +58,44 @@ let j = 0;
 
 //generate word frequency edits
 function generateWordFrequencyEdits(
-    title = "Word Frequency Suggestion",
-    word = "dummyWord",
-    body = "You used the word ",
-    body2 = " times. That's a lot! Try to find a synonym or restructure your sentences to use alternate words.",
-    number = "22"
-    ) {
-  $(".list-group").eq(0).append(
-    " <a href='#' onclick='selectCard(\"elem" +
-      (j + 2).toString() +
-      "\")' class='list-group-item editing-item list-group-item-action list-group-item-light flex-column align-items-start elem" +
-      (j + 2).toString() +
-      "'>" +
-      "<div class='d-flex w-100 justify-content-between'>" +
-      "<h5 class='mb-1'>" +
-      title +
-      "</h5>" +
-      "<span class='badge badge-secondary badge-pill'>" +
-      number +
-      "</span> </div>" +
-      "<p class='mb-1'>" +
-      body + word + "' " + number + body2 +
-      "</p></a>"
-  );
+  title = "Word Frequency Suggestion",
+  word = "dummyWord",
+  body = "You used the word ",
+  body2 = " times. That's a lot! Try to find a synonym or restructure your sentences to use alternate words.",
+  number = "22"
+) {
+  $(".list-group")
+    .eq(0)
+    .append(
+      " <a href='#' wordattr='" +
+        word +
+        `' typeattr='freq'` +
+        "onclick='selectCard(\"elem" +
+        (j + 2).toString() +
+        "\")' class='list-group-item editing-item list-group-item-action list-group-item-light flex-column align-items-start elem" +
+        (j + 2).toString() +
+        "'>" +
+        "<div class='d-flex w-100 justify-content-between'>" +
+        "<h5 class='mb-1'>" +
+        title +
+        "</h5>" +
+        "<span class='badge badge-secondary badge-pill'>" +
+        number +
+        "</span> </div>" +
+        "<p class='mb-1'>" +
+        body +
+        word +
+        "' " +
+        number +
+        body2 +
+        "</p></a>"
+    );
   j++;
 }
 
 //tokenize text
-function tokenize() {
+function tokenize(text) {
   //quill editor functions
-  const text = quill.getText(0);
 
   //remove punctuation
   var punctuation = /[\.,?!]/g;
@@ -93,8 +110,8 @@ function tokenize() {
 
 //generates editing suggestions
 function generateEdits() {
-
-  const filteredTextArray = tokenize();
+  const text = quill.getText(0);
+  const filteredTextArray = tokenize(text);
 
   //number of words in the array
   var numWords = filteredTextArray.length;
@@ -102,19 +119,24 @@ function generateEdits() {
   //create a dictionary of object counts
   var count = {};
 
-  for (k=0; k<numWords; k++) {
+  for (k = 0; k < numWords; k++) {
     var word = filteredTextArray[k];
     if (count[word]) {
       count[word] += 1;
-    }
-    else {
+    } else {
       count[word] = 1;
     }
   }
 
   for (element in count) {
-    if (count[element] / numWords >= .1 && count[element] > 2) {
-      generateWordFrequencyEdits("Word Frequency Suggestion", element, "You used the word '", " times. That's a lot! Try to find a synonym or restructure your sentences to use alternate words.", count[element]);
+    if (count[element] / numWords >= 0.1 && count[element] > 2) {
+      generateWordFrequencyEdits(
+        "Word Frequency Suggestion",
+        element,
+        "You used the word '",
+        " times. That's a lot! Try to find a synonym or restructure your sentences to use alternate words.",
+        count[element]
+      );
     }
   }
 
@@ -125,29 +147,30 @@ function generateEdits() {
   number = "22";
 
   if (j < 6) {
-
-  for (let i = 0; i < numOfSuggestions; i++) {
-
-    $(".list-group").eq(0).append(
-      " <a href='#' onclick='selectCard(\"elem" +
-        (j + 2).toString() +
-        "\")' class='list-group-item editing-item list-group-item-action list-group-item-light flex-column align-items-start elem" +
-        (j + 2).toString() +
-        "'>" +
-        "<div class='d-flex w-100 justify-content-between'>" +
-        "<h5 class='mb-1'>" +
-        title +
-        "</h5>" +
-        "<span class='badge badge-secondary badge-pill'>" +
-        number +
-        "</span> </div>" +
-        "<p class='mb-1'>" +
-        body +
-        "</p></a>"
-    );
-    j++;
-  } }
-} 
+    for (let i = 0; i < numOfSuggestions; i++) {
+      $(".list-group")
+        .eq(0)
+        .append(
+          " <a href='#' onclick='selectCard(\"elem" +
+            (j + 2).toString() +
+            "\")' class='list-group-item editing-item list-group-item-action list-group-item-light flex-column align-items-start elem" +
+            (j + 2).toString() +
+            "'>" +
+            "<div class='d-flex w-100 justify-content-between'>" +
+            "<h5 class='mb-1'>" +
+            title +
+            "</h5>" +
+            "<span class='badge badge-secondary badge-pill'>" +
+            number +
+            "</span> </div>" +
+            "<p class='mb-1'>" +
+            body +
+            "</p></a>"
+        );
+      j++;
+    }
+  }
+}
 
 //delete editing suggestions when clicked away from the editing tab
 function deleteEditingSuggestions() {
@@ -160,21 +183,21 @@ function deleteEditingSuggestions() {
 }
 
 function showButton() {
-    //show the generate AI suggestion button
-      generateButton = $(".generate-button");   
-      for (let i = 0; i < generateButton.length; i++) {   
-        generateButton[i].classList.remove("generate-button-hide");
-        generateButton[i].classList.add("generate-button-show");
-      }
+  //show the generate AI suggestion button
+  generateButton = $(".generate-button");
+  for (let i = 0; i < generateButton.length; i++) {
+    generateButton[i].classList.remove("generate-button-hide");
+    generateButton[i].classList.add("generate-button-show");
+  }
 }
 
 function hideButton() {
   //hide the generate AI suggestion button
-      generateButton = $(".generate-button");      
-      for (let i = 0; i < generateButton.length; i++) {   
-        generateButton[i].classList.remove("generate-button-show");
-        generateButton[i].classList.add("generate-button-hide");
-      }
+  generateButton = $(".generate-button");
+  for (let i = 0; i < generateButton.length; i++) {
+    generateButton[i].classList.remove("generate-button-show");
+    generateButton[i].classList.add("generate-button-hide");
+  }
 }
 
 //tracks number of generated suggestions
@@ -186,34 +209,34 @@ var i = 0;
 function generateAISuggestion(
   body = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   title = "new AI suggestion"
-) {  
-
+) {
   //quill editor functions
   const text = quill.getText(0);
 
   //TODO FOR THE BACKEND: instead of console.logging the text, send to Claude!
   console.log(text);
-
   title = "output from Claude";
   body = "output from Claude";
-
-  $(".list-group").eq(1).append(
-    " <a href='#' onclick='selectCard(\"elem" +
-      (i + 6).toString() +
-      "\")' class='list-group-item list-group-item-action list-group-item-light flex-column align-items-start elem" +
-      (i + 6).toString() +
-      "'>" +
-      "<div class='d-flex w-100 justify-content-between'>" +
-      "<h5 class='mb-1'>" +
-      title +
-      "</h5>" +
-      "</div>" +
-      "<p class='mb-1'>" +
-      body +
-      "</p></a>"
-  );
+    
+  $(".list-group")
+    .eq(1)
+    .append(
+      " <a href='#' onclick='selectCard(\"elem" +
+        (i + 6).toString() +
+        "\")' class='list-group-item list-group-item-action list-group-item-light flex-column align-items-start elem" +
+        (i + 6).toString() +
+        "'>" +
+        "<div class='d-flex w-100 justify-content-between'>" +
+        "<h5 class='mb-1'>" +
+        title +
+        "</h5>" +
+        "</div>" +
+        "<p class='mb-1'>" +
+        body +
+        "</p></a>"
+    );
   i++;
-} 
+}
 
 //makes suggestions selectable
 function selectCard(elemNumber) {
@@ -225,7 +248,47 @@ function selectCard(elemNumber) {
     listItems[i].classList.remove("active");
   }
 
+  resetHighlights();
+
   // Add 'active' tag for currently selected item
   var element = document.querySelector("." + elemNumber); //.getElementById(elemNumber);
   element.classList.add("active");
+
+  console.log(element);
+  // get type of card
+  if (element.getAttribute("typeattr") === "freq") {
+    const selectedWord = element.getAttribute("wordattr");
+    highlightWord(quill.getText(0), selectedWord);
+  }
+}
+
+function resetHighlights() {
+  // Unhighlight currently highlighted text
+  const currentlyHighlighted = document.querySelectorAll(
+    "span[style='color: red;']"
+  );
+  currentlyHighlighted.forEach((element) => {
+    element.style["color"] = "#000";
+  });
+}
+
+function getIndicesOfSubstring(str, substring) {
+  const startingIndices = [];
+  let indexOccurence = str.indexOf(substring, 0);
+
+  while (indexOccurence >= 0) {
+    startingIndices.push(indexOccurence);
+
+    indexOccurence = str.indexOf(substring, indexOccurence + 1);
+  }
+  return startingIndices;
+}
+
+function highlightWord(str, selectedWord) {
+  // Reset other highlights
+
+  const startingIndices = getIndicesOfSubstring(str, selectedWord);
+  for (startIndex of startingIndices) {
+    quill.formatText(startIndex, selectedWord.length, "color", "red");
+  }
 }
